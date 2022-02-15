769a705c739027f1ec489e20058a163d9a014ef0
tjdwls4068 <ssj4068@gmail.com>
2022-02-15 13:45:36 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// ESM
+// COMMON.JS
+// import autoprefixer from 'autoprefixer'
+
+// // export {
+//   plugin: [
+//     autoprefixer
+//   ]
+// }
+
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
